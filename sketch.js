436c3328614f24e418d21481dd343d5f62425185
jas2006 <73657523+jas2006@users.noreply.github.com>
2020-12-01 18:52:13 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,47 +1,52 @@
-var helicopterIMG, helicopterSprite, packageSprite,packageIMG;
-var packageBody,ground
+
 const Engine = Matter.Engine;
 const World = Matter.World;
 const Bodies = Matter.Bodies;
 const Body = Matter.Body;
-var box1 , box2  , box3
+
+var ball , ground , boxLeft , boxRight , boxBottom
 function preload()
 {
-	helicopterIMG=loadImage("helicopter.png")
-	packageIMG=loadImage("package.png")
+	
 }
 
 function setup() {
 	createCanvas(800, 700);
-	rectMode(CENTER);
-	
-
-	packageSprite=createSprite(width/2, 80, 10,10);
-	packageSprite.addImage(packageIMG)
-	packageSprite.scale=0.2
-
-	helicopterSprite=createSprite(width/2, 200, 10,10);
-	helicopterSprite.addImage(helicopterIMG)
-	helicopterSprite.scale=0.6
-
-	groundSprite=createSprite(width/2, height-35, width,10);
-	groundSprite.shapeColor=color(255)
 
 
 	engine = Engine.create();
 	world = engine.world;
+	ballBody = Bodies.circle(700,590,50);
+	ballBody.shapeColor = color(255,0,255);
+	groundBody = Bodies.rectangle(width/2 ,600,800,10);
+	groundBody.shapeColor = color(255,255,0)
+	ball = ellipse(700,590,50);
+	world.add
+	ground = rect(width/2 , 600,800,10);
+	
+	 
+	boxPosition = width/2 - 100
+    boxY = 610
+    boxleftsprite = createSprite(boxPosition,boxY,20,100);
+    boxleftsprite.shapeColor = color(255,0,0);
 
-	packageBody = Bodies.circle(width/2 , 200 , 5);
-	World.add(world, packageBody);
-	//
+    boxLeftBody = Bodies.rectangle(boxPosition+20, boxY, 20,100 , {isStatic:true} );
+    World.add(world, boxLeftBody);
 
-	//Create a Ground
-	ground = Bodies.rectangle(width/2, 650, width, 10 , {isStatic:true} );
-	 World.add(world, ground);
-	 fill("red");
-	box1 = new Box(375,640,50,10);
-	box2 = new Box(375,590,10,50);
-    box3 = new Box(425,590,10,50);
+  	boxbasesprite = createSprite(boxPosition+100, boxY+40, 200,20);
+  	boxbasesprite.shapeColor = color(255,0,0);
+
+  	boxBottomBody = Bodies.rectangle(boxPosition+100, boxY+45-20, 200,20 , {isStatic:true} );
+ 	World.add(world, boxBottomBody);
+
+ 	boxrightsprite = createSprite(boxPosition+200 , boxY, 20,100)
+ 	boxrightsprite.shapeColor = color(255,0,0);
+
+	boxRightBody = Bodies.rectangle(boxPosition+200-20 , boxY, 20,100 , {isStatic:true} );
+ 	World.add(world, boxRightBody);
+    
+	//Create the Bodies Here.
+
 
 	Engine.run(engine);
   
@@ -49,29 +54,18 @@ function setup() {
 
 
 function draw() {
-	background(0);
   rectMode(CENTER);
+  background(0);
   
-  packageSprite.x= packageBody.position.x 
-  packageSprite.y= packageBody.position.y 
-  if (keyCode === "down") {
-	// Look at the hints in the document and understand how to make the package body fall only on press of the Down arrow key.
-   
-	//Matter.Bodies.setStatic(packageBody, isStatic = false);
-	packageBody.restitution = 1.0
+  if (keyCode === UP_ARROW) {
+   ball.velocityY = -10;
+  } 
+  ball.velocityY = ball.velocityY + 0.8;
+    
+  if(keyCode === space){
+	  ball.velocityX = 4 ; 
   }
-  keyPressed();
-  box1.display();
-  box2.display();
-  box3.display();
   drawSprites();
-
  
 }
-
-function keyPressed() {
-	
-}
-
-
 
